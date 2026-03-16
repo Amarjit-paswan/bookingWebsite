06cd5fe0ba5bc18d81b46fbb54ba8da708b4e428
amarjitp594@gmail.com <Amarjit-paswan>
2026-03-16 15:47:15 +0530

    feat: add application entry point (public/index.php)

--- public/index.php
+++ public/index.php
@@ -1,6 +1,19 @@
 <?php 
 
 
+//load router class
+require_once './app/core/Router.php';
+
+//create a router object
+$router = new Router();
+
+//Load the route definitions
+require_once './app/routes/web.php';
+
+//Handle the current request
+$router->resolve();
+
+
 
 
 ?>
